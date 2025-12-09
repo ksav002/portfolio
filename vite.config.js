8d4bd18dfcc2addf769cdf433ba734e5d15baf7d
Keshab Poudel <ksavpoudel2@gmail.com>
2025-12-09 11:54:13 +0545

    Added CNAME and optimized vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  base: "/",
+  build: {
+    minify: "esbuild",
+    cssMinify: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("react-icons")) {
+            return "icons";
+          }
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
